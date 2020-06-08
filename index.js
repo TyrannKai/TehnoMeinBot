8ce7979d3e9499fa562ce5bffd7b9f1983751e34
Tehno <tyrannkai@gmail.com>
2020-06-08 17:15:22 +0300

    Phase 1.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var TelegramBot = require('node-telegram-bot-api');
 var token = '1131647585:AAHBZlzAdFkWapYZ_zlBgwPGogK-h0LYSHU';
 var bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, function (msg, match){
+    var fromID = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromID, resp);
+});
+
+bot.on('message', function (msg){
+    var chatId = msg.chat.id;
+    var photo = 'tehno.png';
+    bot.sendPhoto(chatId, photo, { caption: 'ПК'});
+});
